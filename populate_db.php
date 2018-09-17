6199c1606b584f5e0f2855aa39b8d1f63b934128
Lukas <lukasjurevicius1@gmail.com>
2018-09-17 17:32:58 +0300

    adding php faker to easier populate the database

---
+++ populate_db.php
@@ -0,0 +1,16 @@
+<?php
+// require the Faker autoloader
+require_once '/path/to/Faker/src/autoload.php';
+// alternatively, use another PSR-0 compliant autoloader (like the Symfony2 ClassLoader for instance)
+
+// use the factory to create a Faker\Generator instance
+$faker = Faker\Factory::create();
+
+// generate data by accessing properties
+echo $faker->name;
+
+echo $faker->address;
+
+echo $faker->text;
+
+ ?>

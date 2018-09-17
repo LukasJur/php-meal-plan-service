77c63ae732f90b731f36a1115bf7e94358b805fc
Lukas <lukasjurevicius1@gmail.com>
2018-09-17 23:26:36 +0300

    db connection and implementing random data generator

--- populate_db.php
+++ populate_db.php
@@ -1,16 +1,40 @@
 <?php
 // require the Faker autoloader
-require_once '/path/to/Faker/src/autoload.php';
+require_once 'X:\php_mealplan\php-meal-plan-service\vendor\autoload.php';
 // alternatively, use another PSR-0 compliant autoloader (like the Symfony2 ClassLoader for instance)
 
 // use the factory to create a Faker\Generator instance
 $faker = Faker\Factory::create();
 
 // generate data by accessing properties
-echo $faker->name;
 
-echo $faker->address;
+// random gender
+$gnd = $faker->randomElement($array = array ("male", "female") );
+echo $gnd . " " ;
 
-echo $faker->text;
+//random name 
+if ($gnd == "male"){
+	$frst = $faker->firstNameMale;
+}else{
+	$frst = $faker->firstNameFemale;
+}
+$contacts = $frst. " " . $faker->lastName;
+echo $contacts . " ";
+
+// random age
+echo $faker->numberBetween($min = 15, $max = 100) . "\n" ;
+
+// random height
+echo $faker->numberBetween($min = 140, $max = 200) . "\n";
+
+// random weight
+echo $faker->numberBetween($min = 40, $max = 140) . "\n";
+
+// random act_lvl
+echo $faker->numberBetween($min = 1, $max = 3) . "\n";
+
+// random goal
+echo $faker->randomElement($array = array ("lose", "maintain", "gain") ) ;
+
 
  ?>
